c57b9d5935be310b1519a4b8987f7eedd7b78fb1
t1moon <timurchik1024@gmail.com>
2016-11-20 23:23:19 +0300

    create user

--- db_app/helper/helpers.py
+++ db_app/helper/helpers.py
@@ -1,8 +1,10 @@
 from django.http import JsonResponse
 
+from db_app.queries.forum import SELECT_FORUM_DATA_BY_SLUG
 from db_app.queries.post import SELECT_POST_DATA_BY_ID
 from db_app.queries.profile import SELECT_PROFILE_DATA_BY_EMAIL, SELECT_FOLLOWERS_BY_EMAIL, SELECT_FOLLOWINGS_BY_EMAIL, \
     SELECT_SUBSCRIPTIONS_BY_EMAIL
+from db_app.queries.thread import SELECT_THREAD_DATA_BY_ID
 
 
 def error_json_response(code, response):
@@ -11,6 +13,19 @@ def error_json_response(code, response):
 
 def ok_json_response(code, response):
     return JsonResponse({'code': code, 'response': response})
+
+
+def get_forum_by_slug(cursor, slug):
+    cursor.execute(SELECT_FORUM_DATA_BY_SLUG, [slug, ])
+    forum = cursor.fetchone()
+    return {"id": forum[0],
+            "name": forum[1],
+            "short_name": forum[2],
+            "user": forum[3]
+           }, \
+           {
+            "user": forum[3]
+           }
 
 
 def get_profile_by_email(cursor, user_email):
@@ -64,6 +79,29 @@ def get_post_by_id(cursor, post_id):
          "thread": post[13],
          "user": post[14]
          }
+
+
+def get_thread_by_id(cursor, thread_id):
+    cursor.execute(SELECT_THREAD_DATA_BY_ID, [thread_id, ])
+    thread = cursor.fetchone()
+    return {"date": thread[0].strftime("%Y-%m-%d %H:%M:%S"),
+            "dislikes": thread[1],
+            "forum": thread[2],
+            "id": thread[3],
+            "isClosed": thread[4],
+            "isDeleted": thread[5],
+            "likes": thread[6],
+            "message": thread[7],
+            "points": thread[8],
+            "posts": thread[9],
+            "slug": thread[10],
+            "title": thread[11],
+            "user": thread[12]
+            }, \
+            {
+             "forum": thread[2],
+             "user": thread[12]
+            }
 
 
 
--- db_app/thread/views.py
+++ db_app/thread/views.py
@@ -1,3 +1,15 @@
+from _mysql import connection, IntegrityError
+from json import loads
+
+from django.http import JsonResponse
+
+from db_app.helper import codes
+from db_app.helper.helpers import get_profile_by_email, get_thread_by_id, get_forum_by_slug
+from db_app.queries.forum import SELECT_FORUM_ID_BY_SLUG
+from db_app.queries.profile import SELECT_PROFILE_BY_EMAIL
+from db_app.queries.thread import INSERT_THREAD
+
+
 def close_thread(request):
     pass
 
@@ -7,10 +19,75 @@ def open_thread(request):
 
 
 def create(request):
-    pass
+    params = loads(request.body)
+    forum_name = params['forum']
+    title = params['title']
+    is_closed = params['isClosed']
+    date = params['date']
+    message = params['message']
+    slug = params['slug']
+    email = params['user']
+    try:
+        is_deleted = params['isDeleted']
+    except KeyError:
+        is_deleted = False
+
+    cursor = connection.cursor()
+    # get forum id
+    cursor.execute(SELECT_FORUM_ID_BY_SLUG, [forum_name, ])
+    if cursor.rowcount == 0:
+        return JsonResponse({'code': codes.NOT_FOUND, 'response': 'Forum not found'})
+    forum_id = cursor.fetchone()[0]
+    # get user id
+    cursor.execute(SELECT_PROFILE_BY_EMAIL, [email, ])
+    if cursor.rowcount == 0:
+        return JsonResponse({'code': codes.NOT_FOUND, 'response': 'User not found'})
+    user_id = cursor.fetchone()[0]
+    # add thread
+    try:
+        cursor.execute(INSERT_THREAD, [forum_name, title, is_closed, email, date, message, slug, is_deleted])
+    except IntegrityError:
+        cursor.close()
+        return JsonResponse({'code': codes.ALREADY_EXIST, 'response': 'Thread already exists'})
+    thread_id = cursor.lastrowid
+    thread = {
+        "date": date,
+        "forum": forum_name,
+        "id": thread_id,
+        "isClosed": is_closed,
+        "isDeleted": is_deleted,
+        "message": message,
+        "slug": slug,
+        "title": title,
+        "user": email
+    }
+    cursor.close()
+    return JsonResponse({'code': codes.OK, 'response': thread})
 
 
 def details(request):
+    try:
+        thread_id = request.GET['thread']
+    except KeyError as key_error:
+        return JsonResponse({'code': codes.INCORRECT_QUERY, 'response': 'Not found: {}'.format(str(key_error))})
+    cursor = connection.cursor()
+    try:
+        thread, related_ids = get_thread_by_id(cursor, thread_id)
+    except TypeError:
+        cursor.close()
+        return JsonResponse({'code': codes.NOT_FOUND, 'response': 'Thread doesn\'t exist'})
+
+    related = request.GET.getlist('related')
+    for related_ in related:
+        if related_ not in ['user', 'forum']:
+            cursor.close()
+            return JsonResponse({'code': codes.INCORRECT_QUERY, 'response': 'Incorrect related parameter'})
+        if related_ is 'user':
+            thread['user'] = get_profile_by_email(cursor, related_ids['user'])
+        if related_ is 'forum':
+            thread['forum'], related_ids_ = get_forum_by_slug(cursor, related_ids['forum'])
+    cursor.close()
+    return JsonResponse({'code': codes.OK, 'response': thread})
     pass
 
 
